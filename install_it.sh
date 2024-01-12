ff586ce3cf2c7905cd7cae28686863d41510772d
Scott Register <sreg@sreg.io>
2024-01-12 14:28:43 -0800

    updated install_it

--- install_it.sh
+++ install_it.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
+# TODO: replace all these with symlinks
+
 #tmux
 cp ./tmux.conf ~/.tmux.conf && echo "tmux configured"
 
 #bash
-cat ./bashrc >> ~/.bashrc && echo "bashrc configured"
+#cat ./bashrc >> ~/.bashrc && echo "bashrc configured"
 
 #vim
-cp -R vim ~/.vim && cp vimrc ~/.vimrc && echo "vim files configured"
-echo "source ~/.bashrc and :PlugInstall in vim"
+#cp -R vim ~/.vim && cp vimrc ~/.vimrc && echo "vim files configured"
+#echo "source ~/.bashrc and :PlugInstall in vim"
+
+#neovim
+#
+
+cp ./local/bin/* ~/.local/bin && echo "bin copied"
